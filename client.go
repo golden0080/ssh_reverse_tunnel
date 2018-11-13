da6cea1be485c8e7a772c1cb407b896a3ea71aa0
Guanghui Chen <gh@turingvideo.com>
2018-11-13 12:05:25 -0800

    complete implementation, no tests yet

--- client.go
+++ client.go
@@ -3,10 +3,17 @@ package ssh_reverse_tunnel
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"sync"
 	"time"
 
 	"golang.org/x/crypto/ssh"
+)
+
+var (
+	CanelledError = errors.New("Cancelled")
 )
 
 // Endpoint is a network location, with host and port.
@@ -34,6 +41,8 @@ type ClientConfig struct {
 type Client struct {
 	*ssh.Client
 
+	clientLock *sync.Mutex
+
 	config ClientConfig
 	done   chan bool
 }
@@ -52,16 +61,26 @@ func tcpConnection(tcpEndpoint Endpoint) (*net.Conn, error) {
 	return &conn, err
 }
 
-func NewClient(config *ClientConfig) *Client {
+func NewClient(config ClientConfig) *Client {
 	return &Client{
 		Client: nil,
-		config: *config,
-		done:   nil,
+		config: config,
+		done:   make(chan bool, 4),
+
+		clientLock: &sync.Mutex{},
 	}
 }
 
 func (c *Client) Connect() (err error) {
-	if c.Client != nil {
+	check := func() bool {
+		c.clientLock.Lock()
+		defer c.clientLock.Unlock()
+		if c.Client != nil {
+			return true
+		}
+		return false
+	}
+	if check() {
 		return nil
 	}
 
@@ -72,9 +91,20 @@ func (c *Client) Connect() (err error) {
 		}
 
 		if err == nil {
+			c.clientLock.Lock()
+			defer c.clientLock.Unlock()
 			c.Client = sshConn
 		}
 	}()
+	ready := false
+	for !ready {
+		select {
+		case <-c.done:
+			break
+		default:
+			ready = true
+		}
+	}
 
 	sshConn, err = sshConnection(c.config.SSHServer, &c.config.ClientConfig)
 	if err != nil {
@@ -82,5 +112,95 @@ func (c *Client) Connect() (err error) {
 	}
 
 	// sshConn.Listen()
+	listener, err := sshConn.Listen("tcp", c.config.Remote.String())
+	if err != nil {
+		return err
+	}
+
+	go c.forward(listener)
+
+	return nil
+}
+
+func (c *Client) duplexCopy(remoteConn, localConn net.Conn) (err error) {
+	chDone := make(chan error, 2)
+
+	// Start remote -> local data transfer
+	go func() {
+		_, err := io.Copy(remoteConn, localConn)
+		chDone <- errors.New(fmt.Sprintf("Remote write error: %s", err.Error()))
+	}()
+
+	// Start local -> remote data transfer
+	go func() {
+		_, err := io.Copy(localConn, remoteConn)
+		chDone <- errors.New(fmt.Sprintf("Local write error: %s", err.Error()))
+	}()
+
+	select {
+	case a := <-c.done:
+		c.done <- a
+		chDone <- CanelledError
+		break
+	case e := <-chDone:
+		err = e
+		break
+	}
+	return
+}
+
+func (c *Client) forward(listener net.Listener) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New(fmt.Sprintf("%v", r))
+		}
+		listener.Close()
+	}()
+
+	config := c.config
+	for {
+		select {
+		default:
+			err = func() error {
+				localConn, err := net.DialTimeout(
+					"tcp", config.Local.String(), config.Local.ConnectTimeout,
+				)
+				if err != nil {
+					return err
+				}
+				defer localConn.Close()
+
+				remoteConn, err := listener.Accept()
+				if err != nil {
+					return err
+				}
+				defer remoteConn.Close()
+
+				return c.duplexCopy(remoteConn, localConn)
+			}()
+
+			if err != nil {
+				log.Printf("Connect Error: %s", err.Error())
+			}
+		}
+	}
+}
+
+func (c *Client) Close() error {
+	full := false
+	for !full {
+		select {
+		case c.done <- true:
+			break
+		default:
+			full = true
+		}
+	}
+
+	c.clientLock.Lock()
+	defer c.clientLock.Unlock()
+	if c.Client != nil {
+		return c.Client.Close()
+	}
 	return nil
 }
